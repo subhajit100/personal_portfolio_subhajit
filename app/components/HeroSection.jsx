267eb80572f0fb97a39291b3b9619750ec7d7739
subhajit100 <asubhajit35@gmail.com>
2023-11-01 21:46:57 +0530

    fix resume link to new URL

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -49,7 +49,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="https://drive.google.com/file/d/1h-8BeabdlWm89GDWIhuPj3mU6KkCkhDy/view"
+              href="https://drive.google.com/file/d/1DlQmSDjswb2w0_Z7hd48L9VWt9Mud80b/view"
               target="_blank"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
