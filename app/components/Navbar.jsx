13d78868ffd14524ae673cf40152291bc3aea13d
subhajit100 <asubhajit35@gmail.com>
2024-04-08 10:03:58 +0530

    resolved bug in navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -43,6 +43,7 @@ const Navbar = () => {
           ) : (
             <button
               onClick={() => setNavbarOpen(false)}
+              onBlur={() => setNavbarOpen(false)}
               className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
             >
               <XMarkIcon className="h-5 w-5" />
